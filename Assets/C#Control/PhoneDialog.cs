733582f08d8f39095ee8259a5239b8144809afc5
3360776475 <3360776475@qq.com>
2024-11-10 15:39:07 +0800

    jidong

---
+++ Assets/C#Control/PhoneDialog.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneDialog : MonoBehaviour
+{
+	public GameObject dialogBox;
+	public Text dialogBoxText;
+	public string signText;
+	private bool isPlayerInSign;
+	private bool eKeyPressedOnce = true;
+	//public Transform characterTransform;  // 指向角色的Transform
+	//public Vector3 offset = new Vector3(0, 3f, 0);  // 偏移量，确保文本在角色上方
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//dialogBox.transform.position = Camera.main.WorldToScreenPoint(characterTransform.position + offset);
+
+		if (Input.GetKeyDown(KeyCode.E) && isPlayerInSign)
+		//if ( isPlayerInSign)
+		{
+			if (!eKeyPressedOnce)
+			{
+
+				dialogBox.SetActive(false);
+				eKeyPressedOnce = true;
+			}
+			else
+			{
+				dialogBox.SetActive(true);
+				if (Input.GetKeyUp(KeyCode.E) && !isPlayerInSign)
+					dialogBox.SetActive(false);
+				eKeyPressedOnce = false;
+			}
+		}
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player")
+			&& other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+		{
+			isPlayerInSign = true;
+
+		}
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player")
+			&& other.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+		{
+			isPlayerInSign = false;
+			dialogBox.SetActive(false);
+		}
+	}
+}

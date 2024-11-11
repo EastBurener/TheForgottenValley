8de42f90a225611b9744689e62a9d8b4c73b5cfa
3360776475 <3360776475@qq.com>
2024-11-11 22:52:56 +0800

    11

---
+++ Assets/C#Control/DoorDia.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDia : MonoBehaviour
+{
+	public GameObject tooltipPanel; // 指向提示框的UI元素
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player")) // 确保只有玩家靠近时才显示提示框
+		{
+			tooltipPanel.SetActive(true); // 显示提示框
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player")) // 确保只有玩家离开时才隐藏提示框
+		{
+			tooltipPanel.SetActive(false); // 隐藏提示框
+		}
+	}
+}
--- Assets/C#Control/slottishi.cs
+++ Assets/C#Control/slottishi.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class slottishi : MonoBehaviour
 {
 	public GameObject tooltipPanel; // 指向提示框的UI元素
+	public float interactionRadius = 2f; // 玩家与门交互的距离
+
+	private bool isPlayerNear = false;
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
@@ -15,7 +18,25 @@ public class slottishi : MonoBehaviour
 		}
 	}
 
-	private void OnTriggerExit2D(Collider2D other)
+	void Update()
+	{
+		// 检测玩家是否在门的交互范围内
+		Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, interactionRadius);
+		isPlayerNear = false;
+		foreach (var hitCollider in hitColliders)
+		{
+			if (hitCollider.CompareTag("Player")) // 假设玩家有一个"Player"标签
+			{
+				isPlayerNear = true;
+				break;
+			}
+		}
+		if (isPlayerNear)
+		{
+			tooltipPanel.SetActive(true);
+		}
+	}
+		private void OnTriggerExit2D(Collider2D other)
 	{
 		if (other.CompareTag("Player")) // 确保只有玩家离开时才隐藏提示框
 		{
